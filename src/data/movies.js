8d9077300cc81ae611c13f8a37966891c66b6ae4
penholder <penhold3r@gmail.com>
2020-09-17 17:11:01 -0300

    primera temporada

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,15 +1,43 @@
 export default [
 	{
-		episode: '5ceIa28JqMUOo3cwU8K5jG',
+		episode: '5ceIa28JqMUOo3cwU8K5jG', // #1
 		movies: ['tt4935158'],
 	},
 	{
-		episode: '62eu5YNCZzxKe8kcPRCZrA',
+		episode: '62eu5YNCZzxKe8kcPRCZrA', // #2
 		movies: ['tt5052448', 'tt4670016', 'tt1821549', 'tt0083907'],
 	},
 	{
-		episode: '5vkz2izCZDV7spoitC05iw',
+		episode: '5vkz2izCZDV7spoitC05iw', // #3
 		movies: ['tt2316204', 'tt5329376', 'tt3896738'],
+	},
+	{
+		episode: '2PJsjhFMVg8JvW9nYPdjnA', // #4
+		movies: ['tt0092890', 'tt0082933'],
+	},
+	{
+		episode: '5Ab1dWIxZnpK9ooRlzHhPB', // #5
+		movies: ['tt4680182', 'tt4805316'],
+	},
+	{
+		episode: '0Nu4ttMvpM9aqWoLinYbq6', // #6
+		movies: ['tt5121816', 'tt5221584'],
+	},
+	{
+		episode: '24BB28brkxSeq7meU5hsV6', // #7
+		movies: ['tt4954522', 'tt3780754'],
+	},
+	{
+		episode: '1T8ooziLkSzFE3Tb11Hwbk', // #8 especial navidad
+		movies: null,
+	},
+	{
+		episode: '1rPeRPjxk9GHNd1rhOmdXv', // #9
+		movies: ['tt4695012', 'tt5278832'],
+	},
+	{
+		episode: '6gaIFf94JUjoQMJsCLdUPD', // #10
+		movies: ['tt3967856', 'tt2980592'],
 	},
 	{
 		episode: '6OjYjSf3ZUM78V6HcUgmSX',
