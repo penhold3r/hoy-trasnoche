f42828ab70a540226ab50f6512020da0b5596c24
penholder <penhold3r@gmail.com>
2020-10-07 13:01:28 -0300

    temporada 2

--- src/data/movies.js
+++ src/data/movies.js
@@ -40,7 +40,63 @@ export default [
 		movies: ['tt3967856', 'tt2980592'],
 	},
 	{
-		episode: '6OjYjSf3ZUM78V6HcUgmSX',
+		episode: '4vxd5oaLJ3taq0uzAQ7GwN', // 1: repetto
+		movies: ['tt5109784', 'tt5013056', 'tt1856101', 'tt6047298', 'tt3409848'],
+	},
+	{
+		episode: '1ZSALNDXuIk3d7Lo9MFX7u', // 2: memoria
+		movies: ['tt5462602', 'tt6265828', 'tt3748172', 'tt5536736', 'tt4334266'],
+	},
+	{
+		episode: '2dgmAbdTCPkCZ0nCMUnpF5', // 3: enanos
+		movies: ['tt0066819', 'tt5592248', 'tt6214928', 'tt5657856'],
+	},
+	{
+		episode: '1KNlMUfousuqQiP8KiTNpB', // 4: historia real
+		movies: ['tt3348730', 'tt3532216', 'tt3183402'],
+	},
+	{
+		episode: '1Bb0ztDVObtkpLDv3lPlye', // 5: empujados
+		movies: ['tt3402236', 'tt5247022', 'tt5161376'],
+	},
+	{
+		episode: '6g29bzq11KWHxFZnaTI6og', // 6: video club
+		movies: ['tt5962210', 'tt5112578'],
+	},
+	{
+		episode: '3tCbQa51aglbPwYXgAtZWP', // 7: racista
+		movies: ['tt6777338', 'tt0491175', 'tt6645774', 'tt6288250'],
+	},
+	{
+		episode: '78A15oYcFTWyoxkQaN7Cgd', // 8: pesadillas
+		movies: ['tt5308322', 'tt4622512', 'tt5990474'],
+	},
+	{
+		episode: '2VMWRgLkrA4XKS4miIT2PT', // 9: quiste
+		movies: ['tt2527336', 'tt4846232', 'tt4443658'],
+	},
+	{
+		episode: '0iqQpjaFAnVFshcM1nRTNW', // 10: cumple
+		movies: ['tt3521126', 'tt2272350'],
+	},
+	{
+		episode: '2FS5l0KNjbQAkAmU4z8v9L', // 11: papá novio
+		movies: null,
+	},
+	{
+		episode: '3BWkAJ8DCvvtSzOydyfgkX', // 12: menores
+		movies: ['tt5825380', 'tt7264080', 'tt1139592', 'tt6304046'],
+	},
+	{
+		episode: '37z356k5vYfT3HmEeCjrXL', // 13: pampita
+		movies: ['tt7308086', 'tt1478964'],
+	},
+	{
+		episode: '368KfgtlPukLICS9nXDz1r', // 14: oscars 2018
+		movies: ['tt7907768'],
+	},
+	{
+		episode: '6OjYjSf3ZUM78V6HcUgmSX', // colitis
 		movies: ['tt8647310', 'tt6199572', 'tt0064921'],
 	},
 ]
